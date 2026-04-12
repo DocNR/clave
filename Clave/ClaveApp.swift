f1ff730d1ee1b9c7ff14492b3bae931dbe2df01e
DocNR <thehypoxicdrive@gmail.com>
2026-04-12 08:43:19 -0400

    Initial Commit

---
+++ Clave/ClaveApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClaveApp.swift
+//  Clave
+//
+//  Created by Daniel Wyler on 4/12/26.
+//
+
+import SwiftUI
+
+@main
+struct ClaveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClaveTests/ClaveTests.swift
@@ -0,0 +1,19 @@
+//
+//  ClaveTests.swift
+//  ClaveTests
+//
+//  Created by Daniel Wyler on 4/12/26.
+//
+
+import Testing
+@testable import Clave
+
+struct ClaveTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
